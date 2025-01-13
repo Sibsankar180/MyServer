2400096c39de345b4556aee1b545389999fc968e
Sibsankar180 <gatestudy00@gmail.com>
2025-01-13 19:23:04 +0530

    server

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req , res) =>{
+
+        if(req.url =='/favicon.ico')  return res.end();
+              
         const log =  `${Date.now()} ${req.url} : new req. received \n`;
         fs.appendFile("./log.txt",log, (err,data) =>{
             switch(req.url){
@@ -12,7 +15,7 @@ const myServer = http.createServer((req , res) =>{
                             res.end("About page");
                             break;
                     case  '/contact-us' :
-                        
+
                             res.end("contact page");
                             break;
                     default : res.end("404 page not found");      
