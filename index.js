5b39758b5b6cf34a531f5a03eaa5568a3a7aec46
Sibsankar180 <gatestudy00@gmail.com>
2025-01-13 18:34:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req , res) =>{
-        const log =  `${Date.now()} : new req. received \n`;
+        const log =  `${Date.now()} ${req.url} : new req. received \n`;
         fs.appendFile("./log.txt",log, (err,data) =>{
-            res.end("WelCome");
+            switch(req.url){
+                    case : '/'
+                            res.end("HomePage");
+                            break;
+                    case : '/about'
+                            res.end("About page");
+                            break;
+                    case : '/contact-us'
+                            res.end("contact page");
+                            break;
+                    default : res.end("404 page not found");      
+                            
+            }
+                
         })
         res.end("Welcome");
 });
 
 myServer.listen(8000, () =>{console.log("ServerStarted")});
+
